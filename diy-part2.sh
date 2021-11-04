6938d9ff0e7e76e997091a63cbb5c491e7fb5da5
Hervey <zjjxwhh@163.com>
2021-11-05 02:43:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+sed -i "159a set network.$1.gateway='192.168.9.1'" package/base-files/files/bin/config_generate
+sed -i "159a set network.$1.dns='127.0.0.1 223.5.5.5 8.8.8.8'" package/base-files/files/bin/config_generate
