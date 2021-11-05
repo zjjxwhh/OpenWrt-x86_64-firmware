7a80e24cddeeafb2ef3efdf1cf2eceea5b04f87d
Hervey <zjjxwhh@163.com>
2021-11-05 20:00:45 +0800

    Update diy-part2.sh
    unset default gateway, dns

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
-sed -i "159a set network.$1.gateway='192.168.9.1'" package/base-files/files/bin/config_generate
-sed -i "159a set network.$1.dns='127.0.0.1 223.5.5.5 8.8.8.8'" package/base-files/files/bin/config_generate
+# sed -i "159a set network.$1.gateway='192.168.9.1'" package/base-files/files/bin/config_generate
+# sed -i "159a set network.$1.dns='127.0.0.1 223.5.5.5 8.8.8.8'" package/base-files/files/bin/config_generate
