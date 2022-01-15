9ae76b9c6e12d58c576572ee30da5eec99d3b687
zjjxwhh <zjjxwhh@163.com>
2022-01-16 02:41:38 +0800

    build: Fix build error

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,5 @@ sed -i '11s/lan/wan/g' package/base-files/files/etc/board.d/99-default_network
 sed -i '12s/wan/lan/g' package/base-files/files/etc/board.d/99-default_network
 # sed -i "159a set network.$1.gateway='192.168.9.1'" package/base-files/files/bin/config_generate
 # sed -i "159a set network.$1.dns='127.0.0.1 223.5.5.5 8.8.8.8'" package/base-files/files/bin/config_generate
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
