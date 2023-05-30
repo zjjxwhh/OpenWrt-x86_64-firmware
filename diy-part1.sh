21679c83c2f992d4a27057604ea2a1e16c87994a
zjjxwhh <zjjxwhh@163.com>
2023-05-30 23:04:04 +0800

    build: Add kernel 6.1

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,6 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/WYC-2020/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
