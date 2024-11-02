916938f09ce65e621120d95caf4bd0ce37d8497d
zjjxwhh <zjjxwhh@163.com>
2024-11-02 19:18:28 +0800

    build: Use luci 18.06

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,10 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# Adjust luci feeds
+sed -i 's/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci/src-git luci https:\/\/github.com\/coolsnowwolf\/luci/' feeds.conf.default
+sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\.05/ s/^/#/' feeds.conf.default
+
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
